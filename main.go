b3105e7608e4781b8086146d3294cd2796d352ca
sb.park <sb.park@hnfincore.world>
2022-01-25 16:17:33 +0900

    test goroutine

--- main.go
+++ main.go
@@ -6,16 +6,21 @@ import (
 )
 
 func main() {
+	c := make(chan string)
 
-	go sexCount("been")
-	go sexCount("sung")
-	time.Sleep(time.Second * 5)
-}
-
-func sexCount(person string) {
-	for i := 0; i < 10; i++ {
-		fmt.Println(person, "is sexy", i)
-		time.Sleep(time.Second)
+	people := [5]string{"been", "sung", "nico", "jerry", "abc"}
+	for _, person := range people {
+		go isSexy(person, c)
 	}
 
+	fmt.Println("Waiting for messages")
+	for i := 0; i < len(people); i++ {
+		fmt.Print("waiting for ", i)
+		fmt.Println(<-c)
+	}
+}
+
+func isSexy(person string, c chan string) {
+	time.Sleep(time.Second * 5)
+	c <- person + " is sexy"
 }
